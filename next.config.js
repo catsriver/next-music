07d5078a7c344c58b637ec9048dfac2a07d6fa91
猫河 <catsriver@163.com>
2022-10-10 06:25:55 +0800

    chore: 提供别名支持

--- next.config.js
+++ next.config.js
@@ -1,7 +1,16 @@
 /** @type {import('next').NextConfig} */
+import path from 'path'
+
 const nextConfig = {
     reactStrictMode: true,
-    swcMinify: true
+    swcMinify: true,
+    webpack: (config) => {
+        config.resolve.alias = {
+            ...config.resolve.alias,
+            '@': path.resolve(__dirname)
+        }
+        return config
+    }
 }
 
 module.exports = nextConfig
